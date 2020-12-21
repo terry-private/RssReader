24c617b0a024460e9a71c4f310c26ed576a017bd
teruhito <ataeteruhito@gmail.com>
2020-12-21 21:42:05 +0900

    Initial Commit

---
+++ RssReader/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RssReader
+//
+//  Created by 若江照仁 on 2020/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
