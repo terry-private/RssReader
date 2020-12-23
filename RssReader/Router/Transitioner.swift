789c37ed58cea99fc453f69cbb2351285554827f
teruhito <ataeteruhito@gmail.com>
2020-12-23 21:58:35 +0900

    Transitioner作成 基本的に全てのVCがこのTransitionerに準拠させます。

---
+++ RssReader/Router/Transitioner.swift
@@ -0,0 +1,47 @@
+//
+//  Transitioner.swift
+//  RssReader
+//
+//  Created by 若江照仁 on 2020/12/23.
+//
+
+import Foundation
+import UIKit
+
+protocol Transitioner where Self: UIViewController {
+    func pushViewController(_ viewController: UIViewController, animated: Bool)
+    func popViewController(animated: Bool)
+    func popToRootViewController(animated: Bool)
+    func popToViewController(_ viewController: UIViewController, animated: Bool)
+    func present(viewController: UIViewController,
+                 animated: Bool,
+                 completion: (() -> ())?)
+    func dismiss(animated: Bool)
+}
+
+extension Transitioner {
+    func pushViewController(_ viewController: UIViewController,
+                            animated: Bool) {
+        guard let nc = navigationController else { return }
+        // FIXME: ↑は強制アンラップで落としてあげた方が良いかもしれない？
+        nc.pushViewController(viewController, animated: animated)
+    }
+
+    func popViewController(animated: Bool) {
+
+    }
+
+    func popToRootViewController(animated: Bool) {
+    }
+
+    func popToViewController(_ viewController: UIViewController, animated: Bool) {
+    }
+
+    func present(viewController: UIViewController, animated: Bool, completion: (() -> ())? = nil) {
+        present(viewController, animated: animated, completion: completion)
+    }
+
+    func dismiss(animated: Bool) {
+        dismiss(animated: animated)
+    }
+}
