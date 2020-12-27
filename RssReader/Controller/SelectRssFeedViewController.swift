1b7697ac62d0c555c53b516327fccea27549d8a6
teruhito <ataeteruhito@gmail.com>
2020-12-27 16:04:04 +0900

    SelectRssFeed　追加 VCとStoryboardを作りました。

---
+++ RssReader/Controller/SelectRssFeedViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SelectRssFeedViewController.swift
+//  RssReader
+//
+//  Created by 若江照仁 on 2020/12/27.
+//
+
+import UIKit
+
+class SelectRssFeedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
