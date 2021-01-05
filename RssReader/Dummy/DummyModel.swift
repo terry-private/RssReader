cb27d78dac04e1581f2a37a9f47c836628b73530
teruhito <ataeteruhito@gmail.com>
2021-01-05 23:39:11 +0900

    Dummy Model&Router新規作成 ダミーは一つのファイルにダミークラスを詰め込みます。

---
+++ RssReader/Dummy/DummyModel.swift
@@ -0,0 +1,45 @@
+//
+//  DummyModel.swift
+//  RssReader
+//
+//  Created by 若江照仁 on 2021/01/05.
+//
+
+import Foundation
+
+
+
+class DummyUserConfig: UserConfigProtocol {
+    var userID: String?
+    var photoURL: URL?
+    var displayName: String?
+    var latestLoginDate: Date?
+    func removeUser() {
+        userID = nil
+        photoURL = nil
+        displayName = nil
+        latestLoginDate = nil
+    }
+}
+
+
+
+/// 必ずオートログインに失敗するやつです。
+class DummyLoginModel: LoginProtocol {
+    var userConfig: UserConfigProtocol
+    
+    var autoLoginDelegate: AutoLoginDelegate?
+    init() {
+        userConfig = DummyUserConfig()
+    }
+    func autoLogin() {
+        autoLoginDelegate?.didAutoLogin(isSuccess: false)
+    }
+    
+    func setUserConfig(userID: String, photoURL: URL?, displayName: String) {
+        userConfig.userID = userID
+        userConfig.photoURL = photoURL
+        userConfig.displayName = displayName
+        userConfig.latestLoginDate = Date()
+    }
+}
