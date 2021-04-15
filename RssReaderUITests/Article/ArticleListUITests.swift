2d907f21486babd31705486837a022bcbd44b8ee
teruhito <ataeteruhito@gmail.com>
2021-04-15 16:03:26 +0900

    test: ArticleListUITests作成

---
+++ RssReaderUITests/Article/ArticleListUITests.swift
@@ -0,0 +1,22 @@
+//
+//  ArticleListUITests.swift
+//  RssReaderUITests
+//
+//  Created by 若江照仁 on 2021/04/15.
+//
+
+import XCTest
+
+class ArticleListUITests: XCTestCase {
+    let app = XCUIApplication()
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+        app.launch()
+    }
+
+
+    func testAfterLogin() throws {
+        
+    }
+
+}
