8b192c5cc58db4269822b9149f8ab1b17421ca63
teruhito <ataeteruhito@gmail.com>
2021-04-21 17:54:22 +0900

    test: テーブルビュー側のテスト完了 isLaterReadを拾うためにfunc isFirstTableCellLaterRead() -> Boolを作成

--- RssReaderUITests/Article/StarListUITests.swift
+++ RssReaderUITests/Article/StarListUITests.swift
@@ -22,10 +22,7 @@ class StarListUITests: XCTestCase {
         // コレクションビューのテスト
         collectionViewCellTest()
         
-        // tableViewへの切り替え動作
-        MainTabBar().settingBar.tap()
-        SettingViewPage().displayModeSegmentedControl.buttons.firstMatch.tap()
-        MainTabBar().starListBar.tap()
+        toTableView()
         
         // テーブルのテスト
         tableCellTest()
@@ -124,6 +121,121 @@ class StarListUITests: XCTestCase {
     }
     // MARK:- テーブルのテスト
     private func tableCellTest() {
+        
+        // test 196
+        // 記事セル右スワイプの動作確認
+        XCTContext.runActivity(named: "test 196") { _ in
+            starListPage.tableFirstCell.view.swipeRight()
+            XCTAssertTrue(starListPage.tableFirstCell.starButton.exists)
+            XCTAssertTrue(starListPage.tableFirstCell.laterReadButton.exists)
+            starListPage.tableFirstCell.view.tap()
+        }
+        
+        // test 197 198 204 205
+        // 条件によってtestの順番が入れ替わるので関数を定義しておきます。
+        func test197() {
+            // 記事セル左スワイプ（未読時）の動作確認
+            XCTContext.runActivity(named: "test 197") { _ in
+                starListPage.tableFirstCell.view.swipeLeft()
+                XCTAssertTrue(starListPage.tableFirstCell.readButton.exists)
+            }
+        }
+        func test198() {
+            // 記事セル左スワイプ（既読時）の動作確認
+            XCTContext.runActivity(named: "test 198") { _ in
+                starListPage.tableFirstCell.view.swipeLeft()
+                XCTAssertTrue(starListPage.tableFirstCell.unReadButton.exists)
+            }
+        }
+        func test204() {
+            // チェックボタンの動作確認
+            XCTContext.runActivity(named: "test 204") { _ in
+                starListPage.tableFirstCell.readButton.tap()
+                XCTAssertTrue(starListPage.tableFirstCell.isRead)
+            }
+        }
+        func test205() {
+            // 未読にするボタンの動作確認
+            XCTContext.runActivity(named: "test 205") { _ in
+                starListPage.tableFirstCell.unReadButton.tap()
+                XCTAssertFalse(starListPage.tableFirstCell.isRead)
+            }
+        }
+        if starListPage.tableFirstCell.isRead {
+            // 既読の場合
+            test198()   // 未読にするボタンが出てくる
+            test205()   // 押すと未読になる
+            test197()   // チェックボタンが出てくる
+            test204()   // 押すと既読になる
+        } else {
+            // 未読の場合
+            test197()   // チェックボタンが出てくる
+            test204()   // 押すと既読になる
+            test198()   // 未読にするボタンが出てくる
+            test205()   // 押すと未読になる
+        }
+        
+        
+        // test 201
+        // スターボタン（お気に入り時）の動作確認
+        XCTContext.runActivity(named: "test 201") { _ in
+            let firstCellArticleTitle = starListPage.tableFirstCell.articleTitleLabel.label
+            starListPage.tableFirstCell.view.swipeRight()
+            starListPage.tableFirstCell.starButton.tap()
+            XCTAssertNotEqual(starListPage.tableFirstCell.articleTitleLabel.label, firstCellArticleTitle)
+        }
+        
+        
+        // test 202 203
+        // 条件によってtestの順番が入れ替わるので関数を定義しておきます。
+        func test202() {
+            // 後で読むボタン（後で読む記事の場合）の動作確認
+            XCTContext.runActivity(named: "test 202") { _ in
+                starListPage.tableFirstCell.view.swipeRight()
+                starListPage.tableFirstCell.laterReadButton.tap()
+                XCTAssertFalse(isFirstTableCellLaterRead())
+            }
+        }
+        func test203() {
+            // 後で読むボタン（後で読む記事では無い場合）の動作確認
+            XCTContext.runActivity(named: "test 203") { _ in
+                starListPage.tableFirstCell.view.swipeRight()
+                starListPage.tableFirstCell.laterReadButton.tap()
+                XCTAssertTrue(isFirstTableCellLaterRead())
+            }
+        }
+        if isFirstTableCellLaterRead() {
+            test202()
+            test203()
+        } else {
+            test203()
+            test202()
+        }
     }
-
+    
+    // tableViewへの切り替え動作
+    func toTableView() {
+        MainTabBar().settingBar.tap()
+        SettingViewPage().displayModeSegmentedControl.buttons["line.horizontal.3.decrease"].tap()
+        MainTabBar().starListBar.tap()
+    }
+    
+    // collectionViewへの切り替え動作
+    func toCollectionView() {
+        MainTabBar().settingBar.tap()
+        SettingViewPage().displayModeSegmentedControl.buttons["grid 2x2"].tap()
+        MainTabBar().starListBar.tap()
+    }
+    
+    // isLaterReadはコレクションビューのコンテクストメニューが表示されている時のみ作動するので、
+    // 一旦設定画面でディスプレイモードをコレクションビューの表示に変更して
+    // 対象セルのコンテクストメニューを開いて、isLaterReadを拾ってまたテーブルビューの表示に戻します。
+    func isFirstTableCellLaterRead() -> Bool{
+        toCollectionView()
+        starListPage.collectionViewFirstCell.view.press(forDuration: 1)
+        let isLaterRead = starListPage.collectionViewFirstCell.isLaterRead
+        app.tap()
+        toTableView()
+        return isLaterRead
+    }
 }
