1a0200ad73a2d0c58ddd44792a9067bfc0d0dec5
teruhito <ataeteruhito@gmail.com>
2021-05-21 17:51:58 +0900

    docs: タイポ修正

--- RssReader/Model/WebAPI/Either.swift
+++ RssReader/Model/WebAPI/Either.swift
@@ -11,10 +11,10 @@ import Foundation
 /// たとえば、Either<String, Int> は文字列か整数のどちらかを意味する。
 /// なお、慣例的にどちらの型かを左右で表現することが多い。
 enum Either<Left, Right> {
-    /// Eigher<A, B> の A の方の型。
+    /// Either<A, B> の A の方の型。
     case left(Left)
 
-    /// Eigher<A, B> の B の方の型。
+    /// Either<A, B> の B の方の型。
     case right(Right)
 
 
