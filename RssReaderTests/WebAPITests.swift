305e207ca2055bd9c74514b9517cbcf992e29f9d
teruhito <ataeteruhito@gmail.com>
2021-05-21 12:27:41 +0900

    test: WebAPITests作成

---
+++ RssReaderTests/WebAPITests.swift
@@ -0,0 +1,16 @@
+//
+//  WebAPITests.swift
+//  RssReaderTests
+//
+//  Created by 若江照仁 on 2021/05/21.
+//
+
+import XCTest
+
+class WebAPITests: XCTestCase {
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+}
