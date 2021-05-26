da8db99c06d8b991329801868a04caa91586542d
teruhito <ataeteruhito@gmail.com>
2021-05-26 18:57:24 +0900

    feat: CouponMap作成　色が見にくいので変更してます。

---
+++ RssReader/Controller/CouponMap/CouponMapViewController.swift
@@ -0,0 +1,15 @@
+//
+//  CouponMapViewController.swift
+//  RssReader
+//
+//  Created by 若江照仁 on 2021/05/26.
+//
+
+import UIKit
+import GoogleMaps
+
+class CouponMapViewController: UIViewController, Transitioner {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
